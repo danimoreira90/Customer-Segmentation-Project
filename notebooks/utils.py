c491fc9efcb0879bed3a0de1fb502403fa7f287e
Dani Moreira <danielcm90@gmail.com>
2025-01-28 11:56:36 -0300

    pca visualizations implemented

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from matplotlib.colors import ListedColormap
 
 def elbow_and_silhouette(X, random_state=42, k_range=(2, 11)):
 
@@ -33,3 +34,43 @@ def elbow_and_silhouette(X, random_state=42, k_range=(2, 11)):
 
     plt.show()
  
+
+def show_clusters(
+        dataframe,
+        columns,
+        color_amount,
+        centroids, 
+        show_centroids=False, 
+        show_points=True,
+        cluster_column=None):
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111, projection="3d")
+
+    colors = plt.cm.tab10.colors[:6]
+    colors = ListedColormap(colors)
+
+    x = dataframe[columns[0]]
+    y = dataframe[columns[1]]
+    z = dataframe[columns[2]]
+
+    catch_centroids = show_centroids
+    catch_points = show_points
+
+    for i, centroid, in enumerate(centroids):
+        if catch_centroids:
+            ax.scatter(*centroid, s=500, alpha=0.5)
+            ax.text(*centroid, f"{i}", fontsize=20, horizontalalignment="center", verticalalignment="center")
+
+        if catch_points:
+            s = ax.scatter(x, y, z, c=cluster_column, cmap=colors)
+            ax.legend(*s.legend_elements(), bbox_to_anchor=(1.3, 0.5))
+
+    ax.set_xlabel(columns[0])
+    ax.set_ylabel(columns[1])
+    ax.set_xlabel(columns[2])
+    ax.set_title("Customer Segmentation")
+
+
+    plt.show()
